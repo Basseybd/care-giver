d4a503eae672d64b844ebe865ec6d0b332a903a5
David William Nartey <nartey.dw@gmail.com>
2019-11-25 17:05:39 -0500

    Sample App
    Notifications and Bluetooth permissions enabled. Working on CoreLocation

---
+++ Tabbed/Pods/Target Support Files/Pods-Tabbed/Pods-Tabbed-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tabbed : NSObject
+@end
+@implementation PodsDummy_Pods_Tabbed
+@end
---
+++ Tabbed/Tabbed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabbed
+//
+//  Created by David William Nartey on 11/24/19.
+//  Copyright © 2019 CareGiver. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
