47334b78d88776584900224a5ef90ee72d6f52bd
David William Nartey <nartey.dw@gmail.com>
2019-12-03 15:36:13 -0500

    delete tabbed

--- CareGiverApp/CareGiverApp/AppDelegate.swift
+++ CareGiverApp/CareGiverApp/AppDelegate.swift
@@ -54,12 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    /**
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-    */
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Tabbed/Pods/Target Support Files/Pods-Tabbed/Pods-Tabbed-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tabbed : NSObject
-@end
-@implementation PodsDummy_Pods_Tabbed
-@end
